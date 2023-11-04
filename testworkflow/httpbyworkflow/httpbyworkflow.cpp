5ec1c3908b481078cfa7355108610bc3fef662ce
chenchao <1024204974@qq.com>
2023-11-04 22:56:01 +0800

    2023年 11月 04日 星期六 22:56:01 CST

--- testworkflow/httpbyworkflow/httpbyworkflow.cpp
+++ testworkflow/httpbyworkflow/httpbyworkflow.cpp
@@ -3,9 +3,10 @@
  * @brief 
  */
 #include <iostream>
+#include <workflow/WFHttpServer.h>/*无名称空间*/
+#include <fstream>      //文件对象
 
-
-
+using std::ifstream;
 using std::cout;
 using std::cin;
 using std::endl;
@@ -20,7 +21,30 @@ using std::cerr;
  */
 int main()
 {
-    cout << "Hello world" << endl;
+    //读取文件中的内容
+    ifstream infile("./test.html");
+    if(!infile){
+        cerr << "check you input file" << endl; 
+    }
+     //定位到文件的最末尾
+    infile.seekg(0,std::ios::end);  
+    //获取文件的大小
+    int size = infile.tellg();
+    //将文件指针重新定位到开头
+    infile.seekg(0,std::ios::beg);
+    //动态分配一个文件大小的基本元素为char的内存
+    char* buffer = new char[size];
+    //一次性从磁盘读取整个文件的内容到内存
+    infile.read(buffer,size);
+
+    WFHttpServer server([buffer](WFHttpTask *task) {
+        task->get_resp()->append_output_body(buffer);
+    });
+
+    if (server.start(8888) == 0) {  // start server on port 8888
+        getchar(); // press "Enter" to end.
+        server.stop();
+    }
     return 0;
 }
 
