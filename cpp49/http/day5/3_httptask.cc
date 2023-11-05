f95b670c25397cba64545f63079d80bc8fe4384d
chenchao <1024204974@qq.com>
2023-11-05 22:56:01 +0800

    2023年 11月 05日 星期日 22:56:01 CST

--- cpp49/http/day5/3_httptask.cc
+++ cpp49/http/day5/3_httptask.cc
@@ -6,90 +6,106 @@
 #include <workflow/HttpMessage.h>
 #include <workflow/HttpUtil.h>
 
-using std::endl;
-using std::cout;
 using std::cerr;
 using std::cin;
+using std::cout;
+using std::endl;
 
 static WFFacilities::WaitGroup waitgroup(1);
-void sighandler(int signum){
+void sighandler(int signum)
+{
     cout << "sig number" << signum << "is comming \n";
     waitgroup.done();
 }
-void httpCallback(WFHttpTask* httpTask){
-    //当回调函数被运行时，已经完成了数据的发送和接受了
+void httpCallback(WFHttpTask *httpTask)
+{
+    // 当回调函数被运行时，已经完成了数据的发送和接受了
     cout << "httpCallback is running " << endl;
-    //1.错误检测
+    // 1.错误检测
     int state = httpTask->get_state();
     int error = httpTask->get_error();
 
     switch(state){
     case WFT_STATE_SYS_ERROR:
-        char buff[1024] = {0};
-        cout << "system error: " << strerror_r(error,buff,1024) << endl; break;
+        cout << "system error: " << strerror(error) << endl;
+         break;
     case WFT_STATE_DNS_ERROR:
-        cout << "dns error: " << gai_strerror(error) << endl; break;
+        cout << "dns error: " << gai_strerror(error) << endl;
+         break;
     case WFT_STATE_SUCCESS:
         break;
     }
-
-    if(state != WFT_STATE_SUCCESS){
+    /*switch (state)
+    {
+    case WFT_STATE_SYS_ERROR:
+        printf("system error: %s\n", strerror(error));
+        break;
+    case WFT_STATE_DNS_ERROR:
+        printf("dns error: %s\n", gai_strerror(error));
+        break;
+    case WFT_STATE_SUCCESS:
+        break;
+    }*/
+    if (state != WFT_STATE_SUCCESS)
+    {
         cout << "error occurs " << endl;
-        return ;
-    } 
-    
+        return;
+    }
     cout << "task success! " << endl;
-   
-    //2.获取请求报文的信息
-    //2.1起始行
-    auto req = httpTask->get_req();
-    cout << req->get_method() << req->get_request_uri() 
-    << req->get_http_version() << endl;
 
-    //2.2首部字段，需要通过一个迭代器进行遍历
+    // 2.获取请求报文的信息
+    // 2.1起始行
+    auto req = httpTask->get_req();
+    cout << req->get_method() << req->get_request_uri()
+         << req->get_http_version() << endl;
+
+    // 2.2首部字段，需要通过一个迭代器进行遍历
     protocol::HttpHeaderCursor cursor(req);
-    std::string key ,value;
-    while(cursor.next(key,value)){
+    std::string key, value;
+    while (cursor.next(key, value))
+    {
         cout << key << ": " << value << endl;
     }
     cout << endl;
 
-    //3.获取响应报文的信息
+    // 3.获取响应报文的信息
     auto resp = httpTask->get_resp();
-    //3.1起始行
-    cout << resp->get_http_version();       //http版本信息
-    cout << resp->get_status_code();        //状态码
-    cout << resp->get_reason_phrase();      //原因短语
+    // 3.1起始行
+    cout << resp->get_http_version() << " ";  // http版本信息
+    cout << resp->get_status_code() << " " ;   // 状态码
+    cout << resp->get_reason_phrase()<<endl; // 原因短语
 
-    //3.2首部字段
+    // 3.2首部字段
     protocol::HttpHeaderCursor cursor2(resp);
-    while(cursor2.next(key,value)){
+    while (cursor2.next(key, value))
+    {
         cout << key << ": " << value << endl;
     }
 
-    //3.3消息体
-    const void* body = nullptr;
+    // 3.3消息体
+    const void *body = nullptr;
     size_t sz = 0;
-    resp->get_parsed_body(&body,&sz);   
-    cout << *static_cast<const char*>(body) << endl;
-
+    resp->get_parsed_body(&body, &sz);
+    cout << *static_cast<const char *>(body) << endl;
 }
 /* @brief @pram @return */
-void test(){
-    //注册信号
-    signal(SIGINT,sighandler);
+void test()
+{
+    // 注册信号
+    signal(SIGINT, sighandler);
 
-    //创建任务
-    std::string url = "www.baidu.com";
-    WFHttpTask* httpTask = WFTaskFactory::create_http_task(url,1,1,httpCallback);
+    // 创建任务
+    std::string url = "http://www.baidu.com";
+    WFHttpTask *httpTask = WFTaskFactory::create_http_task(url, 1, 1, 
+        httpCallback);
 
-    //设置任务属性
+    // 设置任务属性
     auto req = httpTask->get_req();
-    req->add_header_pair("User-Agent","workflow http client");
+    req->add_header_pair("User-Agent", "workflow http client");
 
-    //启动任务
+    // 启动任务
     httpTask->start();
-    //阻塞等待
+    // 阻塞等待
     waitgroup.wait();
 }
 int main()
@@ -97,4 +113,3 @@ int main()
     test();
     return 0;
 }
-
--- cpp49/http/day5/4_redistask.cc
+++ cpp49/http/day5/4_redistask.cc
@@ -1,16 +1,86 @@
 /*@file @brief*/
-
+#include <signal.h>
 #include <iostream>
-
+#include <string>
+#include <vector>
+#include <workflow/WFTaskFactory.h>
+#include <workflow/RedisMessage.h>
+#include <workflow/WFFacilities.h>
+using std::vector;
 using std::endl;
 using std::cout;
 using std::cerr;
 using std::cin;
+using std::string;
+static WFFacilities::WaitGroup waitgroup(1);
 
+void sighandler(int signum){
+    cout << "signum " << signum << "is comming" << endl;
+    waitgroup.done();
+}
 
+void redisCallback(WFRedisTask* redisTask){
+    cout<< "redisCallback is running" << endl;
+    //1.错误的检测
+    int state = redisTask->get_state();
+    int error = redisTask->get_error();
+
+    switch(state){
+        case WFT_STATE_DNS_ERROR:
+            cerr << "dns error: " << gai_strerror(error) << endl;
+            break;
+        case WFT_STATE_SYS_ERROR:
+            cerr << "system error: " << strerror(error) << endl;
+            break;
+        case WFT_STATE_SUCCESS:
+            cout << "task sucess" << endl;
+            break;
+    }
+    if(state != WFT_STATE_SUCCESS){
+        cerr << "error occurs" << endl;
+    }
+
+    //获取请求信息
+    auto req = redisTask->get_req();
+    string command;
+    vector<string> params;
+    req->get_command(command);
+    req->get_params(params);
+    cout << "command: ";
+    for(auto& e:params){
+        cout << e << " " ;
+    }
+    cout << endl;
+
+    //获取响应信息
+    auto resp = redisTask->get_resp();
+    protocol::RedisValue result;
+    resp->get_result(result); //const成员函数
+
+    //遍历RedisValue的数据
+    if(result.is_string()){
+        cout << "value is string, value: " 
+            << result.string_value() << endl;
+    }
+    
+
+}
 /* @brief @pram @return */
 void test(){
+    //注册信号
+    signal(SIGINT,sighandler);
 
+    //创建任务
+    std::string redisUrl = "redis://127.0.0.1:6379";
+    WFRedisTask* redisTask = WFTaskFactory::create_redis_task(redisUrl,1
+        ,redisCallback);
+    //设置任务属性
+    auto redisReq = redisTask->get_req();
+    redisReq->set_request("set",{"lwh","123"}); //set {"lwh","123"}
+    //启动框架
+    redisTask->start();
+    //阻塞等待
+    waitgroup.wait();
 }
 int main()
 {
