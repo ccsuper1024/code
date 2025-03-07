956370f95093460d9619a5fd8e18e2b612c70c9f
chenchao <1024204974@qq.com>
2025-03-07 22:56:02 +0800

    2025年 03月 07日 星期五 22:56:01 CST

--- testc++/testmap.cc
+++ testc++/testmap.cc
@@ -6,7 +6,7 @@ int main()
 
     std::map<std::string, int> val = {{"a", 1}, {"b", 2}};
     for(auto it = val.begin(); it != val.end(); ++it)  {
-        std::cout << "it's key = " << it->first << " it'value" << it->second << std::endl;
+        std::cout << "it's key = " << it->first << " it\'value" << it->second << std::endl;
     }
     return 0;
 }
